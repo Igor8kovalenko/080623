7527b1a6f9edaf5ccc607848d5f55200630dd714
Dell-PC <Igorek.kovalenko.2015@mail.ru>
2023-06-08 22:59:03 +0300

    коммитим и отправляем на github

--- str_func.py
+++ str_func.py
@@ -1,7 +1,8 @@
 def capitalize(s):
     result = ""
     capitalize_next = True
-"""реклама"""
+    """реклама"""
+    """добавил комментарий к функции 2"""
     for i in range(len(s)):
         if s[i] == " ":
             result += " "
@@ -17,3 +18,18 @@ def capitalize(s):
 
 
     return result
+def capitalize_naive_one(sentence: str) -> str:
+    """Capitalizes the first letters and doesn't preserve the spaces"""
+
+    words = sentence.split()
+
+    for index_word, word in enumerate(words):
+        word_list = split_char_by_char_list(word)
+        for index_letter, letter in enumerate(word_list):
+            if index_letter == 0:
+                word_list[index_letter] = letter.upper()
+                word = "".join(word_list)
+                break
+        words[index_word] = word
+
+    return " ".join(words)
